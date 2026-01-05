3f23292b2a9d40ee06f6c431b3404d73b2f7ba9f
leclerc92 <leclerc92@gmail.com>
2026-01-05 20:58:38 +0100

    Initial Commit

---
+++ TabPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabPlayer
+//
+//  Created by clement leclerc on 05/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabPlayer/TabPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabPlayerApp.swift
+//  TabPlayer
+//
+//  Created by clement leclerc on 05/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TabPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
